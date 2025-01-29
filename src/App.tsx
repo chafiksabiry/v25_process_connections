919a8e46ba9d5801300c30515f81d4a6ab474920
hosam02 <houssam.qotbi@esi.ac.ma>
2025-01-29 14:41:30 +0100

    premier version de l'application main

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Router>
+      
+
+        <Routes>
+          <Route path="/app1" element={<div>Loading App 1...</div>} />
+          <Route path="/app2" element={<div>Loading App 2...</div>} />
+          <Route path="/app3" element={<div>Loading App 3...</div>} />
+          <Route path="/app4" element={<div>Loading App 4...</div>} />
+        </Routes>
+      
+    </Router>
+  );
+};
+
+export default App;
