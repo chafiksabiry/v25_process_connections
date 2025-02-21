1c9f9f9379fb6fb0e74828b47932ec7e01486c08
hosam02 <houssam.qotbi@esi.ac.ma>
2025-02-21 18:56:50 +0100

    possible fix to the cors issue

--- vite.config.ts
+++ vite.config.ts
@@ -27,8 +27,12 @@ export default defineConfig(({ mode }) => {
     ],
     server: {
       port: 3000,
-      
       cors: true, 
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, DELETE',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
     },
     optimizeDeps: {
       include: ['react', 'react-dom'], // Pre-bundle react and react-dom to ensure smooth behavior
