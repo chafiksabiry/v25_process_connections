b0202dee29b25b086da82e8a51a1d7d1e2a1debd
El Houcine Qara <elhoucine.qara@harx.ai>
2025-05-22 14:46:21 +0100

    Add app-12

--- src/main.tsx
+++ src/main.tsx
@@ -236,11 +236,6 @@ const startQiankun = async () => {
         return fetch(url, {
           ...options,
           mode: "cors",
-          credentials: "include",
-          headers: {
-            ...options?.headers,
-            "Origin": "https://v25.harx.ai",
-          },
         });
       },
     });
