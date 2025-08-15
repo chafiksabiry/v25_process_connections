9cb68cc89bc46948619f821295b750d874d0eaa9
BILON Ikram <bilonikram20@gmail.com>
2025-08-15 16:02:32 +0100

    add copilot configs

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,7 @@ const App = () => {
         <Route path="/gigs" element={<div id="container-gigs"></div>} />
         <Route path="/company" element={<div id="container-company"></div>} />
         <Route path="/app12" element={ <div id="container-app12"></div>}/>
+        <Route path="/copilot" element={ <div id="container-copilot"></div>}/>
       </Routes>
     </Router>
   );
