94fd8d5401c2a2fdd3560e9206a25798369387ab
elhoucineqara114 <elhoucine.qara@harx.ai>
2026-06-26 21:31:00 +0100

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    resolve: {
-        alias: {
-            '@harx/shared': path.resolve(__dirname, '../shared'),
-        },
-    },
     server: {
         port: 5173,
         strictPort: true,
-        fs: {
-            allow: [path.resolve(__dirname, '..')],
-        },
     },
     build: {
         outDir: 'dist',
